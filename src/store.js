c86256922bf5c4a01929abe15286fb765af1f458
MatheusReis <matheus.reis@efabrika.com.br>
2019-10-06 22:04:22 -0300

    Add vuex store

--- src/store.js
+++ src/store.js
@@ -1,16 +1,39 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import repository from './api'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    temperature: 50,
+    humidity: 50
   },
   mutations: {
-
+    SET_TEMPERATURE (state, payload) {
+      state.temperature = payload
+    },
+    SET_HUMIDITY (state, payload) {
+      state.humidity = payload
+    }
   },
   actions: {
-
+    async getGrowData ({ commit }) {
+      let temp, hum;
+      try {
+        let data = await repository.getData()
+        console.log(`data fetch >>`, data)
+        temp = data.temp
+        hum  = data.hum
+      } catch (error) {
+        temp, hum = 0
+        console.error(error)
+      } finally {
+        commit('SET_TEMPERATURE', temp)
+        commit('SET_HUMIDITY', hum)
+      }
+      
+    }
   }
 })
