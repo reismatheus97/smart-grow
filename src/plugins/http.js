db24a4c8293164bf8c620fb05a6098df9c3ce632
Matheus <reismatheus97@gmail.com>
2019-10-27 19:27:33 -0300

    Update baseURL

--- src/plugins/http.js
+++ src/plugins/http.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 // Sets the default url used by all of this axios instance's requests
-axios.defaults.baseURL = 'http://192.168.0.15'
+axios.defaults.baseURL = 'http://192.168.0.125'
 
 export default axios
