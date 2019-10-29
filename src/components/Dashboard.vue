c789421d16aef30344d2f44b2b8e6d9ce78006a8
MatheusReis <matheus.reis@efabrika.com.br>
2019-10-29 03:52:54 -0300

    Beta version

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -4,7 +4,7 @@
     <v-row align="center">
       <v-col offset="1" cols="10" align="center">
         <v-row justify="center">
-          <v-col xs12 sm="2">
+          <v-col xs="12" sm="2">
             <v-icon class="welcome-icon d-flex">
               fa-seedling
             </v-icon>
@@ -16,10 +16,10 @@
           </v-col>
         </v-row>
         <v-divider class="mt-5"></v-divider>
-        <v-row align="center" >
+        <v-row align="center" class="flex-column flex-sm-row">
           <v-col sm="12" class="ma-auto pa-0">
-            <v-row>
-              <v-col>
+            <v-row class="flex-sm-row flex-wrap" justify="center">
+              <v-col sm="5" md="3">
                 <v-progress-circular
                   :rotate="-90"
                   :size="200"
@@ -37,7 +37,7 @@
                 </v-progress-circular>
               </v-col>
               <v-divider vertical></v-divider>
-              <v-col>
+              <v-col sm="5" md="3">
                 <v-progress-circular
                   :rotate="-90"
                   :size="200"
@@ -56,59 +56,100 @@
                   </div>
                 </v-progress-circular>
               </v-col>
+              <v-divider vertical></v-divider>
+              <v-col sm="5" md="3">
+                <v-progress-circular
+                  :rotate="-90"
+                  :size="200"
+                  :width="12"
+                  :value="temperature * 2"
+                  color="orange lighten-1"
+                  class="my-3"
+                >
+                  <div class="display-1">
+                    <v-icon class="task__icon--progress my-4">
+                      fa-thermometer-half
+                    </v-icon>
+                    <div class="display-1">{{ temperature }}<span class="title font-weight-regular">ºC</span></div>
+                  </div>
+                </v-progress-circular>
+              </v-col>
             </v-row>
           </v-col>
         </v-row>
         <v-divider></v-divider>
-        <v-row align="center" >
+        <v-row align="center" class="flex-column flex-sm-row">
           <v-col sm="3" class="ma-auto">
             <v-row class="my-5">
-              <v-col>
-                <v-icon v-if="Number(ledState)" class="task__icon" color="yellow lighten-3">
-                  fa-lightbulb
-                </v-icon>
-                <v-icon v-else class="task__icon">
-                  far fa-lightbulb
-                </v-icon>
+              <v-col class="task__icon--container">
+                <div>
+                  <transition name="fade-transition">
+                    <v-icon v-if="Number(ledState)" class="task__icon" color="yellow lighten-3">
+                      fa-lightbulb
+                    </v-icon>
+                    <v-icon v-else class="task__icon">
+                      far fa-lightbulb
+                    </v-icon>
+                  </transition>
+                </div>
+                <v-switch v-model="ledState" inset color="green accent-2" readonly :loading="loading" class="d-inline-block"></v-switch>
               </v-col>
             </v-row>
             <v-row>
-              <div class="headline font-weight-light">
-                Iluminação <v-switch v-model="ledState" inset color="green lighten-2" :loading="loading" class="d-inline-block"></v-switch>
-              </div>
+              <div class="headline py-2">Iluminação</div>
+              <v-btn-toggle>
+                <v-btn class="task__btn" @click="toggleComponent('off', 'led')">
+                  OFF
+                </v-btn>
+                <v-btn class="task__btn" @click="toggleComponent('on', 'led')">
+                  ON
+                </v-btn>
+              </v-btn-toggle>
             </v-row>
           </v-col>
           <v-divider vertical></v-divider>
           <v-col sm="3" class="ma-auto">
             <v-row class="my-5">
-              <v-col>
-                <v-icon class="task__icon" :class="{ spinning: Number(fanState) }">
-                  fa-fan
-                </v-icon>
+              <v-col class="task__icon--container">
+                <div>
+                  <v-icon class="task__icon" :class="{ spinning: Number(fanState) }">
+                    fa-fan
+                  </v-icon>
+                </div>
+                <v-switch v-model="fanState" inset color="green accent-2" readonly :loading="loading" class="d-inline-block"></v-switch>
               </v-col>
             </v-row>
-            <v-row>
-              <div class="headline font-weight-light">
-                Exaustão <v-switch v-model="fanState" inset color="green lighten-2" :loading="loading" class="d-inline-block"></v-switch>
-              </div>
+            <v-row px-2>
+              <div class="headline py-2">Exaustão</div>
+              <v-btn-toggle>
+                <v-btn class="task__btn" @click="toggleComponent('off', 'fan')">
+                  OFF
+                </v-btn>
+                <v-btn class="task__btn" @click="toggleComponent('on', 'fan')">
+                  ON
+                </v-btn>
+              </v-btn-toggle>
             </v-row>
           </v-col>
           <v-divider vertical></v-divider>
           <v-col sm="3" class="ma-auto">
             <v-row class="my-5">
-              <v-col>
-                <v-icon class="task__icon" :class="{ 'blue--text text--lighten-1': wpState} ">
-                  fa-shower
-                </v-icon>
+              <v-col class="task__icon--container">
+                <div>
+                  <v-icon class="task__icon" :class="{ 'blue--text text--lighten-1': wpState} ">
+                    fa-shower
+                  </v-icon>
+                </div>
+                <v-switch v-model="fanState" inset color="green accent-2" readonly :loading="loading" class="d-inline-block"></v-switch>
               </v-col>
             </v-row>
             <v-row>
-              <div class="headline font-weight-light">
-                Bomba d'água
-              </div>
-            </v-row>
-            <v-row>
-              <v-switch v-model="fanState" inset color="blue lighten-2"></v-switch>
+              <div class="headline py-2 d-inline-block text-truncate">Irrigação</div>
+              <v-btn-toggle>
+                <v-btn class="task__btn" @click="toggleComponent('on', 'led')">
+                  PULSAR
+                </v-btn>
+              </v-btn-toggle>
             </v-row>
           </v-col>
         </v-row>
@@ -129,7 +170,7 @@ export default {
       this.toggleLoading()
       await this.$store.dispatch(`getGrowData`)
       this.toggleLoading()
-    }, 8000)
+    }, 15000)
   },
   data: () => ({
     loading: false
@@ -151,11 +192,11 @@ export default {
       get () {
         return Number(this.$store.state.ledState)
       },
-      set (val) { this.toggleComponent(val, "led") }
+      // set (val) { this.toggleComponent(val, "led") }
     },
     fanState: {
       get () { return Number(this.$store.state.fanState) },
-      set (val) { this.toggleComponent(val, "fan") }
+      // set (val) { this.toggleComponent(val, "fan") }
     },
     wpState: {
       get () { return this.$store.state.wpState },
@@ -203,9 +244,46 @@ export default {
     color: #81C784 !important;
   }
 
+  .v-btn:before {
+    background-color: unset !important;
+  }
+
+  .v-input--selection-controls__ripple {
+    cursor: unset !important;
+  }
+
+  .v-input--selection-controls__input > input {
+      cursor: unset !important;
+  }
+
+  .task {
+    &__btn > .v-btn:focus::before {
+      background-color: unset !important;
+    }
+
+    &__btn {
+      display: flex !important;
+      flex-grow: 1 !important;
+      background-color: rgb(49, 49, 49) !important;
+    }
+
+    &__btn:hover {
+      transform: scale(1.02) !important;
+    }
+  }
+
   .task__icon {
     font-size: 110px !important;
     margin-bottom: 10px;
+    display: block !important;
+
+    &--container {
+      max-height: 165px !important;
+
+      & > div {
+        height: 125px;
+      }
+    }
 
     &--progress {
       font-size: 75px !important;
